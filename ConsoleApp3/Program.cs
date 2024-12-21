fa7b2f0f93bf9bef485e7e8a943bb2b1ace07ea0
Kommandor <a.tsymbaliuk2007@gmail.com>
2024-12-21 13:07:29 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("It's easy to win forgiveness for being wrong;");
+            Console.WriteLine("being right is what gets you into real trouble.");
+            Console.WriteLine("Bjarne Stroustrup");
+        }
+    }
+}
